0c6570ccf3a6f837328069b94e2d72f6d5e5bbd5
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2023-11-01 08:53:55 -0300

    Authentication e Authorization

--- JWT/Program.cs
+++ JWT/Program.cs
@@ -1,13 +1,44 @@
 using JWT.Services;
 using JWT.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using JWT;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddTransient<TokenService>();
+
+
+// Sempre adicionar esse Midware para trabalhar com autenticação
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+
+});
+
+//Sempre utilizar o Authorization depois do Authentication
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
-app.MapGet("/", (TokenService service) 
+//Sempre adicionar esse Midware (Antes das Rotas) para trabalhar com autenticação
+app.UseAuthentication();
+
+//Sempre utilizar nessa ordem
+app.UseAuthorization();
+
+app.MapGet("/", (TokenService service)
     =>
     {
         var user = new User(
@@ -16,11 +47,11 @@ app.MapGet("/", (TokenService service)
             "xyz@dan.com",                 // Email
             "123!@#",                      // Password
             "https://www",                 // Image
-            new[] {"student", "premium"}   // Roles
+            new[] { "student", "premium" }   // Roles
         );
 
         return service.Create(user);
-    } 
+    }
 );
 
 app.Run();
